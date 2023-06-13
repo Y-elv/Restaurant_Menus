abe3928f9648a769834e6dd9f233c95a6c95d512
mugisha <mugishaelvis456@gmail.com>
2023-06-13 13:17:37 +0200

    initialisation

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+
+import './App.css'
+import MainLayout from './Layout/MainLayout'
+
+function App() {
+  
+
+  return (
+    <>
+    <MainLayout/>
+    <div className="main">
+    <h3 id="title">Today's specials</h3>
+    <div className="landingpage">
+      
+     <div className="image"></div>
+    </div>
+    </div>
+   
+ <div className="product-container">
+  
+ </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,17 @@
+import '../Css/mainlayout.css'
+import img1 from '../assets/menu-alt-left-regular-24.png'
+import img2 from '../assets/search-alt-2-regular-24.png'
+const MainLayout =()=>{
+    return(
+        <>
+       <div className="container">
+        <div className="menu"><img src={img1}/></div>
+        <div className="text">
+            <h3>Tall bakery cafe</h3>
+        </div>
+        <div className="search"><img src={img2}/></div>
+       </div>
+        </>
+    )
+}
+export default MainLayout
