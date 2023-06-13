549697e4ab58a25f8abedfb4f7eaa45ef7041dcd
mugisha <mugishaelvis456@gmail.com>
2023-06-13 18:10:03 +0200

    adding card components

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,46 @@
 
 import './App.css'
 import MainLayout from './Layout/MainLayout'
+import BigCard from './components/bigCard'
+import SmallCard from './components/SmallCard'
+
+
 
 function App() {
-  
+
 
   return (
     <>
-    <MainLayout/>
-    <div className="main">
-    <h3 id="title">Today's specials</h3>
-    <div className="landingpage">
-      
-     <div className="image"></div>
-    </div>
-    </div>
-   
- <div className="product-container">
-  
- </div>
+      <MainLayout />
+      <div className="main">
+        <h3 id="title">Today's specials</h3>
+        <div className="landingpage">
+
+          <div className="image"></div>
+        </div>
+      </div>
+
+      <div className="product-container">
+        <div className="middle">
+          <div className="upper">
+            <h4>Menu</h4>
+            <h4>See all</h4>
+          </div>
+          <div className="lower">
+            <h4>Soupus</h4>
+            <h4>Desserts</h4>
+            <h4>Appertizers</h4>
+            <h4>Drink</h4>
+          </div>
+
+        </div>
+        <div className="product-card">
+         <BigCard/>
+         
+         
+        </div>
+
+      </div>
     </>
   )
 }
---
+++ src/components/SmallCard.jsx
@@ -0,0 +1,13 @@
+import '../CSs/smallcard.css'
+const SmallCard =()=>{
+    return( 
+    <>
+    <div className="all">
+        <div className="left"></div>
+        <div className="right"></div>
+    </div>
+    </>
+    )
+   
+}
+export default SmallCard
